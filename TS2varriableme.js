04aabce157480a032081ed98c21c3a11ca20794d
saamirkc <samir.kc.a20@gmail.com>
2024-10-12 00:11:43 +0545

    commit

---
+++ TS2varriableme.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello";
+console.log(greetings);
+//number 
+var userId = 3345;
+// boolean
+var isLoggedIn = false;
